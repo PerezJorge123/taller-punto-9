54e3d64baefb4a0812e39c182b3667f644a3a298
Jorge <Jorge@DESKTOP-MSOVDS0>
2023-09-06 22:51:03 -0500

    Solucion punto 9

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Construir un programa que pida por pantalla 3 números y luego diga cuál es el
+// mayor de los números ingresados.
+public class TallerPunto9
+{
+    static void Main(string[] args)
+    {
+
+        double num1, num2, num3; // Datos de entrada: num1, num2, num3
+
+        Console.Write("Ingrese el primer numero:"); // Pedir numeros
+        num1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el segundo numero:");
+        num2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el tercer numero:");
+        num3 = double.Parse(Console.ReadLine());
+
+        if (num1 > num2 && num1 > num3) //Proceso / impresion del resultado
+        {
+            Console.WriteLine($"El número {num1} es mayor:");
+        } 
+        else if (num2 > num3 && num2 > num1)
+        {
+            Console.WriteLine($"El número {num2} es mayor:");
+        }
+        else
+        {
+            Console.WriteLine($"El número {num3} es mayor:");
+        }
+    }
+}
